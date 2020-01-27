18a0e58c8e6a5d24b6a0c2d8b5f48d51a3e89d28
Amia Defreitas <amia.defreitas0@gmail.com>
2020-01-27 09:47:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+  
+
+function addingEventListener(event) {
+    const input = document.querySelector('input');
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
